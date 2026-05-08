c184c6454407c99a7a85f0aeef2e3c3eb5c42c8d
Heptazhou <zhou@0h7z.com>
2026-05-08 03:26:27 +0000

    Update

--- snow_launcher_downloader.js
+++ snow_launcher_downloader.js
@@ -1,49 +1,48 @@
 async function download_launcher(mode) {
-	let server_name = document.querySelector("input[name='server']:checked")?.value || "jinshan"
-	let launcher_version = document.querySelector("input[name='version']")?.value || "2.0.0"
+	let server_name = document.querySelector("input[name='server']:checked")?.value || "seasun"
+	let launcher_version = document.querySelector("input[name='version']")?.value || "latest"
 
 	const progressBar = document.getElementById("progress_bar")
 	const statusText = document.getElementById("status")
 
 	let url = ""
 
-	function fetch_latest_version(check_url) {
-		return fetch(check_url)
-			.then((response) => response.text()) // 先将response转为文本
-			.then((text) => {
-				// 获取文本（版本号）并将其赋值给 launcher_version 变量，去除末尾的换行符
-				launcher_version = text.replace("\n", "")
-				console.log(`Latest version: ${launcher_version}`)
-			})
-			.catch((error) => {
-				console.error("Error fetching version:", error)
-			})
+	async function fetch_latest_version(check_url) {
+		try {
+			const response = await fetch(check_url)
+			const text = await response.text()
+			// 获取文本（版本号）并将其赋值给 launcher_version 变量，去除末尾的换行符
+			launcher_version = text.trim()
+			console.log(`Latest version: ${launcher_version}`)
+		} catch (error) {
+			console.error("Error fetching version:", error)
+		}
 	}
 
 	switch (server_name) {
 		case "jinshan":
 			if (launcher_version === "latest") {
-				await fetch_latest_version("https://cbjq-content.xoyocdn.com/ob202307/launcher/jinshan/updates/latest")
+				await fetch_latest_version(`https://cbjq-content.xoyocdn.com/ob202307/launcher/${server_name}/updates/latest`)
 			}
-			url = `https://cbjq-content.xoyocdn.com/ob202307/launcher/jinshan/updates/${launcher_version}.exe`
+			url = `https://cbjq-content.xoyocdn.com/ob202307/launcher/${server_name}/updates/${launcher_version}.exe`
 			break
 		case "bilibili":
 			if (launcher_version === "latest") {
-				await fetch_latest_version("https://cbjq-content.xoyocdn.com/ob202307/launcher/bilibili/updates/latest")
+				await fetch_latest_version(`https://cbjq-content.xoyocdn.com/ob202307/launcher/${server_name}/updates/latest`)
 			}
-			url = `https://cbjq-content.xoyocdn.com/ob202307/launcher/bilibili/updates/${launcher_version}.exe`
+			url = `https://cbjq-content.xoyocdn.com/ob202307/launcher/${server_name}/updates/${launcher_version}.exe`
 			break
-		case "worldwide":
+		case "seasun":
 			if (launcher_version === "latest") {
-				await fetch_latest_version("https://snowbreak-content.amazingseasuncdn.com/ob202307/launcher/seasun/updates/latest")
+				await fetch_latest_version(`https://snowbreak-content.amazingseasuncdn.com/ob202307/launcher/${server_name}/updates/latest`)
 			}
-			url = `https://snowbreak-content.amazingseasuncdn.com/ob202307/launcher/seasun/updates/${launcher_version}.exe`
+			url = `https://snowbreak-content.amazingseasuncdn.com/ob202307/launcher/${server_name}/updates/${launcher_version}.exe`
 			break
 	}
 
 	// 创建下载链接
 	const a = document.createElement("a")
-	a.download = `Snowbreak_Launcher_${server_name}_v${launcher_version}.exe` // 指定文件名
+	a.download = `snowbreak-${server_name}-v${launcher_version}.exe` // 指定文件名
 
 	if (mode !== "direct") {
 		try {
@@ -92,9 +91,3 @@ async function download_launcher(mode) {
 		a.click()
 	}
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-	let inputBox = document.querySelector("input[type='text'][id='version']")
-
-	inputBox.value = "latest"
-})
